9cd6a13d2505faea2b3aaaf1348ec4c83e01ebcd
Paulus Gunadi <paulus@sg.ibm.com>
2018-06-06 18:57:29 +0800

    update kibana_install

--- variables.tf
+++ variables.tf
@@ -90,6 +90,10 @@ variable "disable_management" {
   default = ["istio", "custom-metrics-adapter"]
 }
 
+variable "kibana_install" {
+  default = "false"
+}
+
 variable "gluster_volume_type" {
   default = "none"
 }
